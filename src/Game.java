c40b1a5bc6dac4c2e6e49b76a3dc44a2a620cbcc
chismoc <chisomo.chingwalu@yahoo.co.uk>
2021-08-12 13:19:49 +0200

    Add a math method in Game class

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,8 @@ public class Game {
     public void Hello() {
         System.out.println("Hello World");
     }
+
+    public int doTheMath(int numb, int numb2) {
+        return numb + numb2;
+    }
 }
